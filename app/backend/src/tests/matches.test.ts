4bb6ef56bafe3a21abe76e9ded42f117d098b8b2
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-08-26 17:51:34 -0300

    Feat: Realização dos testes de integração da rota matches

---
+++ app/backend/src/tests/matches.test.ts
@@ -0,0 +1,48 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import MatchesModel from '../database/models/match.model';
+import MatchesMock from '../tests/mocks/matches.mock';
+import { app } from '../app';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Teste a rota /matches', function () { 
+  beforeEach(function () { sinon.restore(); });
+
+  it('Teste se é possível listar todas as partidas', async function () {
+    sinon.stub(MatchesModel, 'findAll').resolves(MatchesMock as any);
+    const response = await chai.request(app).get('/matches');
+    expect(response.status).to.be.equal(200);
+  });
+
+  it('Teste se filtrar as partidas em progresso', async function () {
+    const matchesInProgress = MatchesMock.filter((match) => match.inProgress === true);
+    sinon.stub(MatchesModel, 'findAll').resolves(MatchesMock as any);
+    const response = await chai.request(app).get('/matches?inProgress=true');
+    expect(response.status).to.be.equal(200);
+  });
+
+  it('Teste se filtrar as partidas finalizadas', async function () {
+    const matchesInProgress = MatchesMock.filter((match) => match.inProgress === false);
+    sinon.stub(MatchesModel, 'findAll').resolves(MatchesMock as any);
+    const response = await chai.request(app).get('/matches?inProgress=false');
+    expect(response.status).to.be.equal(200);
+  });
+
+  // it('Teste se é possível atualizar uma partida', async function () {
+  //   sinon.stub(MatchesModel, 'update').resolves();
+  //   const response = await chai.request(app).patch('/matches/1').send({ homeTeamGoals: 1, awayTeamGoals: 0 });
+  //   expect(response.status).to.be.equal(200);
+  // });
+
+  // it('Teste se é possível atualizar uma partida como finalizada', async function () {
+  //   sinon.stub(MatchesModel, 'update').resolves();
+  //   const response = await chai.request(app).patch('/matches/1/finish');
+  //   expect(response.status).to.be.equal(200);
+  // });
+});
--- app/backend/src/tests/mocks/login.mock.ts
+++ app/backend/src/tests/mocks/login.mock.ts
@@ -3,7 +3,7 @@ const LoginMock = {
   username: 'Admin',
   email: 'admin@admin.com',
   role: 'admin',
-  password: 'secret_admin',
+  password: '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
 }
 
 export default LoginMock;
