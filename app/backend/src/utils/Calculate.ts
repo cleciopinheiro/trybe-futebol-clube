2ebd0f511c152a4abab1209c1251f0ff8d1bbb99
cleciopinheiro <cleciosantosdeveloper@gmail>
2023-08-28 21:17:55 -0300

    Fix: Refatorando codigo

--- app/backend/src/utils/Calculate.ts
+++ app/backend/src/utils/Calculate.ts
@@ -1,45 +1,43 @@
-import ILeaderboard from '../interfaces/ILeaderboard';
+// import ILeaderboard from '../interfaces/ILeaderboard';
 
-const leaderboardModel = () => ({
-  name: '',
-  totalPoints: 0,
-  totalGames: 0,
-  totalVictories: 0,
-  totalDraws: 0,
-  totalLosses: 0,
-  goalsFavor: 0,
-  goalsOwn: 0,
-  goalsBalance: 0,
-  efficiency: 0,
-});
+// const leaderboardModel = (home: ILeaderboard, away: ILeaderboard) => ({
+//   name: home.name,
+//   totalPoints: 0,
+//   totalGames: 0,
+//   totalVictories: 0,
+//   totalDraws: 0,
+//   totalLosses: 0,
+//   goalsFavor: 0,
+//   goalsOwn: 0,
+//   goalsBalance: 0,
+//   efficiency: 0,
+// });
 
-const infosByTeam = (home: ILeaderboard, away: ILeaderboard) => {
-  const team = leaderboardModel();
-  team.name = home.name;
-  team.totalPoints = Number(home.totalPoints) + Number(away.totalPoints);
-  team.totalGames = Number(home.totalGames) + Number(away.totalGames);
-  team.totalVictories = Number(home.totalVictories) + Number(away.totalVictories);
-  team.totalDraws = Number(home.totalDraws) + Number(away.totalDraws);
-  team.totalLosses = Number(home.totalLosses) + Number(away.totalLosses);
-  team.goalsFavor = Number(home.goalsFavor) + Number(away.goalsFavor);
-  team.goalsOwn = Number(home.goalsOwn) + Number(away.goalsOwn);
-  return team;
-};
+// const infosByTeam = (home: ILeaderboard, away: ILeaderboard) => ({
+//   name: home.name,
+//   totalPoints: Number(home.totalPoints) + Number(away.totalPoints),
+//   totalGames: Number(home.totalGames) + Number(away.totalGames),
+//   totalVictories: Number(home.totalVictories) + Number(away.totalVictories),
+//   totalDraws: Number(home.totalDraws) + Number(away.totalDraws),
+//   totalLosses: Number(home.totalLosses) + Number(away.totalLosses),
+//   goalsFavor: Number(home.goalsFavor) + Number(away.goalsFavor),
+//   goalsOwn: Number(home.goalsOwn) + Number(away.goalsOwn),
+// });
 
-const leaderboardGenerate = (home: ILeaderboard[], away: ILeaderboard[]) => {
-  const array: ILeaderboard[] = [];
-  for (let i = 0; i < home.length; i += 1) {
-    for (let j = 0; j < away.length; j += 1) {
-      if (home[i].name === away[j].name) {
-        const team = infosByTeam(home[i], away[j]);
-        team.goalsBalance = team.goalsFavor - team.goalsOwn;
-        team.efficiency = (team.totalPoints / (team.totalGames * 3)) * 100;
-        team.efficiency = +(team.efficiency.toFixed(2));
-        array.push(team as unknown as ILeaderboard);
-      }
-    }
-  }
-  return array;
-};
+// const calculateStatis = (home: ILeaderboard[], away: ILeaderboard[]) => {
+//   const array: ILeaderboard[] = [];
+//   for (let i = 0; i < home.length; i += 1) {
+//     for (let j = 0; j < away.length; j += 1) {
+//       if (home[i].name === away[j].name) {
+//         const team = infosByTeam(home[i], away[j]);
+//         team.goalsBalance = team.goalsFavor - team.goalsOwn;
+//         team.efficiency = (team.totalPoints / (team.totalGames * 3)) * 100;
+//         team.efficiency = +(team.efficiency.toFixed(2));
+//         array.push(team as unknown as ILeaderboard);
+//       }
+//     }
+//   }
+//   return array;
+// };
 
-export default leaderboardGenerate;
+// export default leaderboardGenerate;
